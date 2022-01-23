222b3f8771b6e479998db89df4a7df2524271be1
Mehmet Tarhan <hi@memtarhan.com>
2022-01-23 15:13:17 +0000

    add table view to home screen

---
+++ Birthdays/Screens/Home/Birthday/BirthdayTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BirthdayTableViewCell.swift
+//  Birthdays
+//
+//  Created by Mehmet Tarhan on 23/01/2022.
+//
+
+import UIKit
+
+class BirthdayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Birthdays/Screens/Home/HomeViewController.swift
+++ Birthdays/Screens/Home/HomeViewController.swift
@@ -14,6 +14,16 @@ protocol HomeViewController: AnyObject {
 class HomeViewControllerImpl: UIViewController {
     var presenter: HomePresenter?
 
+    // MARK: - Outlets
+
+    @IBOutlet var tableView: UITableView!
+
+    // MARK: - Properties
+
+    private let cellReuseIdentifier = "Birthday"
+    private let cellNibIdentifier = "BirthdayTableViewCell"
+    private let rowHeight: CGFloat = 96
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -21,10 +31,41 @@ class HomeViewControllerImpl: UIViewController {
 
     private func setup() {
         title = "Birthdays"
+        
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 96
+
+        let cellNib = UINib(nibName: cellNibIdentifier, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: cellReuseIdentifier)
     }
 }
 
 // MARK: - HomeViewController
 
 extension HomeViewControllerImpl: HomeViewController {
+}
+
+// MARK: - UITableViewDataSource
+
+extension HomeViewControllerImpl: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as? BirthdayTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension HomeViewControllerImpl: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
 }
